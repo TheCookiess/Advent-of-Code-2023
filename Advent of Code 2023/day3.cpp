f1da79f4fb280c143e9b18e773c7d93a59430ba0
BuildTools <tomboyyyalt@gmail.com>
2023-12-03 23:44:06 +0000

    day 3 complete.

--- Advent of Code 2023/day3.cpp
+++ Advent of Code 2023/day3.cpp
@@ -2,45 +2,87 @@
 #include "includes.h"
 using namespace std;
 
+			//	//cout << "number captured: " << num << endl;
+			//	//cout << "x: " << x << endl;
+			//	//cout << "y: " << y << endl;
+			//	//for (int i = 0; i < 80; i++)
+			//	//	cout << '=';
+			//	//cout << endl;
+			//
+			//	/*
+			//	for (int i = -1; i <= num.size(); i++) {// check neighbours for whole length of number
+			//		for (int j = -1; j <= 1; j++) { // check top & bot neighbours
+			//			//if (y + i >= input.size() || y + i < 0 || x + j >= line.size() || x + j < 0) continue; // bounds check
+			//			if (input[y + i][x + j - num.size() - 1] != '.') // x is currently char after word. so go back
+			//				summation += strtol(num.c_str(), nullptr, 10);
+			//		}
+			//	}
+			//	for (int i = -1; i < 1; i++)
+			//	for (int j = -1; j < 1; j++) {
+			//		char dC = input[y + i][x + j];
+			//		if (dC == '.') // if adj char is == '.'
+			//			isValid = false;
+			//	}
+			//	*/
+
 int Solutions::d3_p1(vector<string> input)
 {
 	int summation = 0;
-	for (int y = 1; y < input.size() - 1; y++) {
-		string& line = input[y];
-		string num = "";
-		bool isValid = true;
-		for (int x = 1; x < line.size() - 1; x++) {
-			char c = line[x]; // reference (i64) > char (i8)
-			if (isdigit(c)) { // capturing a number (multiple digits)
-				num += c;
-				for (int i = -1; i < 1; i++)
-					for (int j = -1; j < 1; j++) {
-						char dC = input[y + i][x + j];
-						if (dC == '.') // if adj char is == '.'
-							isValid = false;
-					}
-			}
-			else if (!num.empty()) { // number captured, check neighbours for symbols
-				//fo`r (int i = -1; i <= num.size(); i++) {// check neighbours for whole length of number
-				//	f`or (int j = -1; j <= 1; j++) { // check top & bot neighbours
-				//		`//if (y + i >= input.size() || y + i < 0 || x + j >= line.size() || x + j < 0) continue; // bounds check
-				//		`if (input[y + i][x + j - num.size() - 1] != '.') // x is currently char after word. so go back
-				//		`	summation += strtol(num.c_str(), nullptr, 10);
-				//	}`
-				//}
-				if (isValid)
-					summation += strtol(num.c_str(), nullptr, 10);
-				//cout << "number captured: " << num << endl;
-				//cout << "x: " << x << endl;
-				//cout << "y: " << y << endl;
-				//for (int i = 0; i < 80; i++)
-				//	cout << '=';
-				//cout << endl;
+	int yBound = input.size();
+	int xBound = input[0].size(); // all lines are same length
 
-				num = "";
-				isValid = true;
+	auto isCharNumber = [](char c)
+	{ return (c >= '0' && c <= '9'); };
+
+	auto isCharSymbol = [isCharNumber](char c)
+	{ return (!isCharNumber(c) && c != '.'); };
+
+	auto isInBounds = [xBound, yBound](int x, int y)
+	{ return (x >= 0 && y >= 0 && x < xBound && y < yBound);  };
+
+	//auto isNegative = [&input, isInBounds](int x, int y)
+	//{ return (isInBounds(x, y) && input[y][x] == '-'); };
+
+	auto isPart = [&input, isInBounds, isCharSymbol](int x, int y) {
+		for (int dY = -1; dY <= 1; dY++)
+			for (int dX = -1; dX <= 1; dX++)
+				if (isInBounds(x + dX, y + dY) && isCharSymbol(input[y + dY][x + dX])) {
+					return true;
+					//cout << input[y][x] << " has: " << input[y + dY][x + dX] << " as a neighbour" << endl;
+				}
+		return false;
+	};
+
+	auto strToInt = [](string num) {
+		int summation = 0;
+		for (int i = 0; i < num.size(); i++) {
+			summation *= 10;
+			summation += num[i] - '0';
+		}
+		return summation;
+	};
+
+	for (int y = 0; y < input.size(); y++) { // each line
+		string& line = input[y];
+		int num = 0;
+		for (int x = 0; x < line.size(); x++) { // each character
+			if (!isCharNumber(line[x])) continue;
+
+			// have to include first char!
+			int dX = 1;
+			string num; num += line[x];
+			bool isValidPart = isPart(x,y);
+			while (isInBounds(x+dX, y) && isCharNumber(line[x + dX])) { // num.size() acts as a counter technically but ruins readability.
+				num += line[x + dX];
+				if (isPart(x + dX, y)) 
+					isValidPart = true;
+				dX++;
 			}
-			else { num = ""; isValid = true; };
+
+			if (isValidPart)
+				summation += strToInt(num);
+
+			x += dX; // advance past number.
 		}
 	}
 	return summation;
@@ -48,7 +90,72 @@ int Solutions::d3_p1(vector<string> input)
 
 int Solutions::d3_p2(vector<string> input)
 {
-	int summation = 0;
+	long long summation = 0;
+	int yBound = input.size();
+	int xBound = input[0].size(); // all lines are same length
+
+											// note to self: multimaps BAD!!
+	unordered_map<int, vector<int>> umap;	// int1 == absolute index of a gear
+											// vec<int> == all numbers neighbouring the gear.
+
+	auto isCharNumber = [](char c)
+	{ return (c >= '0' && c <= '9'); };
+
+	auto isCharGear = [](char c)
+	{ return (c == '*'); };
+
+	auto isInBounds = [xBound, yBound](int x, int y)
+	{ return (x >= 0 && y >= 0 && x < xBound&& y < yBound);  };
+
+
+	auto absoluteIndex = [xBound](int x, int y)
+	{ return y * xBound + x;  };
+
+	auto strToInt = [](string num) {
+		int summation = 0;
+		for (int i = 0; i < num.size(); i++) {
+			summation *= 10;
+			summation += num[i] - '0';
+		}
+		return summation;
+	};
+
+	// absolute clusterfuckery
+	auto checkForGears = [&input, &umap, isInBounds, isCharGear, absoluteIndex](int x, int y, int num, int length) {
+		for (int dY = -1; dY <= 1; dY++)
+			for (int dX = -1; dX <= length; dX++)
+				if (isInBounds(x + dX, y + dY) && isCharGear(input[y + dY][x + dX])) {
+					int gearIndex = absoluteIndex(x + dX, y + dY);
+					umap[gearIndex].push_back(num);
+					//else
+					//if (umap.contains(gearIndex))
+					//	umap[gearIndex] = {num};
+					return;
+				}
+		return;
+	};
+
+	for (int y = 0; y < input.size(); y++) {
+		string& line = input[y];
+		for (int x = 0; x < line.size(); x++) {
+			if (!isCharNumber(input[y][x])) continue;
+
+			int dX = 1;
+			string num = ""; num += line[x];
+			while (isInBounds(x + dX, y) && isCharNumber(line[x + dX])) { // num.size() acts as a counter technically but ruins readability.
+				num += line[x + dX];
+				dX++;
+			}
+			checkForGears(x, y, strToInt(num), num.size());
+
+			x += dX;
+		}
+	}
+
+	for (const auto& n : umap)
+		if (n.second.size() == 2)
+			summation += (n.second[0] * n.second[1]);
+			//cout << n.second[0] << endl << n.second[1] << endl << "--" << endl;
 
 	return summation;
 }
--- Advent of Code 2023/main.cpp
+++ Advent of Code 2023/main.cpp
@@ -7,38 +7,32 @@ int main()
 {
 	Solutions* AOC = new Solutions;
 	vector<string> lines = AOC->getInput("3");
-
 	int part1 = 0, part2 = 0;
+	
 	constexpr int loops = 1;
 	auto p1_t1 = chrono::high_resolution_clock::now();
-	/* -------------------------------------------------- */
 	for (int i = 0; i < loops; i++)
 		part1 = AOC->d3_p1(lines);
-	/* -------------------------------------------------- */
 	auto p1_t2 = chrono::high_resolution_clock::now();
 	auto p2_t1 = chrono::high_resolution_clock::now();
-	/* -------------------------------------------------- */
 	for (int i = 0; i < loops; i++)
 		part2 = AOC->d3_p2(lines);
-	/* -------------------------------------------------- */
 	auto p2_t2 = chrono::high_resolution_clock::now();
 
 	chrono::duration<double, micro> p1_time = (p1_t2 - p1_t1) / loops;
 	chrono::duration<double, micro> p2_time = (p2_t2 - p2_t1) / loops;
-
+	
 	// all this code for dumb little formatting lol
 	string p1_padding = "", p2_padding = "";
 	int paddingAmt = (to_string(part2).size()) - (to_string(part1).size());
-	unsigned int clampedAmt = paddingAmt; // clamps to positive
 	if (paddingAmt > 0)
-		for (int i = 0; i < clampedAmt; i++) p1_padding += " ";
+		for (int i = 0; i < paddingAmt; i++) p1_padding += " ";
 	else
-		for (int i = 0; i < clampedAmt; i++) p2_padding += " ";
+		for (int i = 0; i < paddingAmt*-1; i++) p2_padding += " ";
 
 	cout << endl;
 	cout << "Solution 1: " << part1 << p1_padding << " Executed in: " << p1_time.count() / 1000 << "ms" << endl;
 	cout << "Solution 2: " << part2 << p2_padding << " Executed in: " << p2_time.count() / 1000 << "ms" << endl;
-	cout << endl;
 
 	return 1;
 }
