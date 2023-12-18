3a52d1e00a153d5e5f6e0ee38ce3be5d8272c87a
BuildTools <tomboyyyalt@gmail.com>
2023-12-18 23:14:33 +0000

    d10 p1 complete.

--- Advent of Code 2023/day10.cpp
+++ Advent of Code 2023/day10.cpp
@@ -10,16 +10,15 @@ enum Direction {
 };
 
 struct Node {
-	int x, y;
+	int x, y, dist;
 	char c;
 	vector<Direction> con;
-	bool checked;
-	Node(int X, int Y, char C) {
+	Node(int X, int Y, char C, vector<Direction> CON) {
 		x = X;
 		y = Y;
 		c = C;
-		con = {};
-		checked = false;
+		con = CON;
+		dist = 0;
 	}
 	Node() = default;
 };
@@ -35,21 +34,20 @@ int64_t Solutions::d10_p1(vector<string> input)
 	 {'J', {NORTH,WEST} }, 
 	 {'7', {SOUTH,WEST} }, 
 	 {'F', {SOUTH,EAST} }, 
+	 {'S', {NORTH,SOUTH,EAST,WEST}},
 	 /*{'.', {}}*/      };
 
 	
 	auto getIdx = [&input](int x, int y) -> int 
-	{ return (y + x) * input[y].size(); };
+	{ return (y * input[y].size()) + x; };
 
 	auto ooB = [&input](int x, int y) -> bool 
 	{ return (x > input.size() || x < 0 || y > input[y].size() || y < 0); };
 
-	auto isInQueue = [](int x, int y, vector<Node>& queue) -> bool {
-		for (Node n : queue) if (n.x == x && n.y == y) return false;
-		return true;
+	auto isInGraph = [](int x, int y, vector<Node>& graph) -> bool {
+		for (Node n : graph) if (n.x == x && n.y == y) return true;
+		return false;
 	};
-
-	auto 
 
 	auto getOppDirection = [](Direction d) -> Direction {
 		switch (d) {
@@ -61,12 +59,12 @@ int64_t Solutions::d10_p1(vector<string> input)
 	};
 
 	auto checkDirection = [&](int x, int y, Direction d) -> bool {
-		switch (d) {
-		case NORTH: y--; break;
-		case EAST:  x++; break;
-		case SOUTH: y++; break;
-		case WEST:  x--; break;
-		}
+		//switch (d) {
+		//case NORTH: y--; break;
+		//case EAST:  x++; break;
+		//case SOUTH: y++; break;
+		//case WEST:  x--; break;
+		//}
 
 		if (ooB(x, y)) return false;					// if is in bounds
 		if (!umap.contains(input[y][x])) return false;	// if is a letter
@@ -76,53 +74,67 @@ int64_t Solutions::d10_p1(vector<string> input)
 		return false;
 	};
 
-	//auto updateNodeConnections = [&](Node& n) -> vector<Direction> {
-	//	if (checkDirection(n.x, n.y, NORTH)) n.con.push_back(NORTH);
-	//	if (checkDirection(n.x, n.y, EAST )) n.con.push_back(EAST );
-	//	if (checkDirection(n.x, n.y, SOUTH)) n.con.push_back(SOUTH);
-	//	if (checkDirection(n.x, n.y, WEST )) n.con.push_back(WEST );
-	//};
+	auto traverseNode = [&](int x, int y, Direction d,vector<Node>& graph, Node&  srcNode) -> void {
+		Node& n = nodes[getIdx(x, y)];
+		if (checkDirection(n.x, n.y, d))
+			if (!isInGraph(n.x, n.y, graph)) {
+				n.dist = srcNode.dist + 1;
+				graph.push_back(nodes[getIdx(n.x, n.y)]);
+				//cout << "Valid Node: "
+				//	<< n.c
+				//	<< " (" << n.x << "," << n.y << ") "
+				//	<< endl;
+			}
+	};
 
 	int startIdx = -1;
 	for (int i = 0; i < input.size(); i++)
 		for (int j = 0; j < input[i].size(); j++) {
-			nodes.emplace_back(j, i, input[i][j]);
+			nodes.emplace_back(j, i, input[i][j], umap[input[i][j]]);
 			if (input[i][j] == 'S')
 				startIdx = getIdx(j, i);
 		}
 	if (startIdx == -1) return 0; // no start. is fucked.
 
-	/* this is fucking useless...
-	* Step 1: Start with node S and enqueue it to the queue.
-	* Step 2: Repeat the following steps for all the nodes in the graph.	
-	* Step 3: Dequeue S and process it.
-	* Step 4: Enqueue all the adjacent nodes of S and process them.
-	* [END OF LOOP]
-	* Step 6: EXIT
-	*/
+	vector<Node> graph; graph.push_back(nodes[startIdx]);
+	int front = 0;
+	while (graph.size() > front) {
+		Node n = graph[front]; // get item at front of graph "queue"
+		cout << "Processing: "
+			<< n.c
+			<< " (" << n.x << "," << n.y << ") "
+			<< endl;
 
-	vector<Node> queue; queue.push_back(nodes[startIdx]);
-	while (!queue.empty()) {
-		Node n = queue[0]; // get item at front of queue
+		for (Direction d : n.con) {
+			switch (d) {
+			case NORTH: traverseNode(n.x, n.y - 1, NORTH, graph, n); break;
+			case SOUTH: traverseNode(n.x, n.y + 1, SOUTH, graph, n); break;
+			case EAST:  traverseNode(n.x + 1, n.y, EAST , graph, n); break;
+			case WEST:  traverseNode(n.x - 1, n.y, WEST , graph, n); break;
+			}
+		}
 
-		if (checkDirection(n.x, n.y, NORTH))
-			if (!isInQueue(n.x, n.y + 1, queue))
-				queue.push_back(nodes[getIdx(n.x, n.y + 1)]);
-		if (checkDirection(n.x, n.y, EAST))
-			if (!isInQueue(n.x + 1, n.y, queue))
-				queue.push_back(nodes[getIdx(n.x + 1, n.y + 1)]);
-		if (checkDirection(n.x, n.y, NORTH))
-			if (!isInQueue(n.x, n.y + 1, queue))
-				queue.push_back(nodes[getIdx(n.x, n.y + 1)]);
-		if (checkDirection(n.x, n.y, NORTH))
-			if (!isInQueue(n.x, n.y + 1, queue))
-				queue.push_back(nodes[getIdx(n.x, n.y + 1)]);
-
+		//cout << endl;
+		front++;
 	}
+
+	int y = 0;
+	for (Node& n : nodes) {
+		if (n.y != y) {
+			y++; cout << endl;
+		}
+		if (isInGraph(n.x, n.y, graph))
+			cout << n.dist;
+		else
+			cout << '.';
+	}
+	cout << endl;
 		 
+	int64_t largest = 0;
+	for (Node n : graph)
+		if (n.dist > largest) largest = n.dist;
 
-
-	return 0;
+	return largest;
 }
 
 int64_t Solutions::d10_p2(vector<string> input)
